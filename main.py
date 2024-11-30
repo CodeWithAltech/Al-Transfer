5413b75596010268ff7abae3bb242b5027083641
Abaasa Albert <codewithaltech@gmail.com>
2024-11-30 22:35:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import requests
 import random
@@ -23,6 +24,14 @@ else:
 
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,         
+    allow_methods=["*"],             
+    allow_headers=["*"],
+)
 
 
 class IPNRegistrationRequest(BaseModel):
